cd644fb89e2d32b5b6ebd9587f4b792dc87e8c55
Andrew Nelson <nelsand19@gmail.com>
2023-05-25 19:09:14 -0600

    finish game funtion and loop

--- script.js
+++ script.js
@@ -14,27 +14,22 @@ Computer wins
 loop to 5 games
 first to 5 wins wins
 */
-let answer = (prompt("Please choose Rock, Paper, or Scissors!"))
 function getPlayerChoice(){
-    console.log(answer)
-    let answerTwo = answer.toUpperCase()
-    return answerTwo
-    console.log(answerTwo)
-}
-const selectionTwo = getPlayerChoice()
-console.log(selectionTwo)
+    let answer = (prompt("Please choose Rock, Paper, or Scissors!"))
+    let selectionTwo = answer.toUpperCase()
+  
+    return selectionTwo
+   
+} 
 
 function getComputerChoice(){
  let result = Math.floor(Math.random()*3+1)
- console.log(result)
  if (result === 1) {return "ROCK"}
  if (result === 2) {return "PAPER"} 
  if (result === 3) {return "SCISSORS"}
 }
-const selection = getComputerChoice()
-console.log(selection)
 
-function playRound(playerSelection, ComputerSelection) {
+function playRound(selection, selectionTwo) {
     if (
         (selectionTwo === "ROCK") && (selection === "SCISSORS") ||
         (selectionTwo === "PAPER") && (selection === "ROCK") ||
@@ -45,19 +40,51 @@ function playRound(playerSelection, ComputerSelection) {
         (selectionTwo === "ROCK") && (selection === "PAPER") ||
         (selectionTwo === "PAPER") && (selection === "SCISSORS") ||
         (selectionTwo === "SCISSORS") && (selection === "ROCK")
-    ){return "Computer Wins!"}4
+    ){return "Computer Wins!"}
 
     if (
         selection === selectionTwo
     ) {return "Its A Tie!"}
+
+    else {return "Not a Valid Selection"}
 }
-console.log(playRound())
 /* game function will start both the player and the computer at 0. then add a
 point to whoever wins the round. then after five rounds declare a winner!*/
 
+/**
+ * Function to run a game of rock paper scissors
+ * @return {string} win, lose, or tie
+ */
 let playerScore = 0
-let ComputerScore = 0
-for (var i=0; i < 6; i++) {
-    console.log(playRound())
-    console.log(playerScore, ComputerScore)
+let computerScore = 0
+function game() {
+    // Create variables to keep track of player score and computer score
+   
+    // Loop 5 times
+    for (var i=0; i < 5; i++) {
+       let player = getPlayerChoice()
+       let computer = getComputerChoice()
+       let result = playRound(player, computer)
+       if (result === "Player Wins!") {
+        playerScore++
+       }
+       if (result === "Computer Wins!") {
+        computerScore++
+       }
+       console.log(result)
+       console.log(playerScore, computerScore)
+    }
+    // Compare player score to computer score, decide a winner
+    if (computerScore > playerScore) {
+        return "Computer Wins!"
+    }
+    if (playerScore > computerScore) {
+        return "Player Wins!"
+    }
+    if (playerScore === computerScore) {
+        return "Its a Tie"
+    }
+    // Return the result of the game
 }
+const gameResult = game()
+console.log(gameResult)
