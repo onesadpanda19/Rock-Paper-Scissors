c5a32cbf34c789932d613c2e05425afb6c9715ac
Andrew Nelson <nelsand19@gmail.com>
2023-07-10 19:02:10 -0600

    add css file, update js file, and update html file

--- script.js
+++ script.js
@@ -12,9 +12,37 @@ Player Wins
 if opposite
 Computer wins
 loop to 5 games
-first to 5 wins wins
+first to 5 wins
 */
+let playerScore = 0
+let computerScore = 0
+let buttons = document.querySelectorAll(".btn");
+let roundResult;
+    if (roundResult =)
+// Global Scope
+buttons.forEach((button) => {
+    // Outer scope
+    button.addEventListener("click", () => {
+        // Inner scope
+        // Get a player choice
+        let player = (button.id) // Rock, paper, scissors
 
+        // Get a computer choice
+        let computer = getComputerChoice()
+        
+        //Play a round
+        roundResult = playRound(computer, player)
+       
+    })
+});
+
+
+
+/**
+ * getComputerChoice is a function that randomly selects either ROCK, PAPER, or SCISSORS
+ * 
+ * @returns {String} returns the string ROCK, PAPER, or SCISSORS
+ */
 function getComputerChoice(){
  let result = Math.floor(Math.random()*3+1)
  if (result === 1) {return "ROCK"}
@@ -48,8 +76,6 @@ point to whoever wins the round. then after five rounds declare a winner!*/
  * Function to run a game of rock paper scissors
  * @return {string} win, lose, or tie
  */
-let playerScore = 0
-let computerScore = 0
 function game() {
     // Create variables to keep track of player score and computer score
    
